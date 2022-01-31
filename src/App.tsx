60b5f130d4a0a4e439c627b1c5c7ce63c00d752a
Armando Cruz <jaimes087.uz@gmail.com>
2022-01-30 21:47:34 -0600

    style(globals): add globals styles (theme variables, resets, fonts)

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/button';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Code something cool :)</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/button.tsx
@@ -0,0 +1,11 @@
+export const Button = () => {
+  return (
+    <button
+      style={{
+        backgrondColor: 'royalblue',
+      }}
+    >
+      Hello!
+    </button>
+  );
+};
