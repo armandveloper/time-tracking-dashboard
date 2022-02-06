6dc98d1e887b7b1736ba744af8fcdfad709a824d
Armando Cruz Jaimes <armando.cruz@digitalvaultstudio.com>
2022-02-05 20:25:02 -0600

    feat: add sidebar header

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,8 +1,18 @@
 import styles from './index.module.css';
+import avatar from '@/assets/image-jeremy.png';
 
 export const Sidebar = () => {
+  const name = 'Jeremy Robson';
+
   return (
     <aside className={styles.sidebar}>
+      <header className={styles.header}>
+        <img className={styles.avatar} src={avatar} alt={name} />
+        <div>
+          <p>Report for</p>
+          <span>{name}</span>
+        </div>
+      </header>
       <nav className={styles.nav}>
         <a className={styles.link} href="!#">
           Daily
